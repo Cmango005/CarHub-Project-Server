4888106cab920f4f55cbf5b1b1390b8756a769ec
Cmango005 <mh410676@gmail.com>
2023-10-24 19:55:47 +0600

    user & google auth server commit

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const productCollection= client.db("brandDB").collection("products")
+    const productCollection= client.db("brandDB").collection("products");
+    const userCollection = client.db("brandDB").collection("user");
     app.get('/products', async(req, res)=>{
         const cursor = productCollection.find();
         const result = await cursor.toArray();
@@ -82,6 +83,16 @@ async function run() {
       }
       const result= await productCollection.updateOne(filter,product,options);
       res.send(result)
+    })
+    app.get('/user', async(req, res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+    app.post('/user', async(req, res)=>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     })
 
 
