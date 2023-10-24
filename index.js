083a2e56cf14820d0f7b67a0bede9b8c63242446
Cmango005 <mh410676@gmail.com>
2023-10-25 04:53:32 +0600

    add to cart server commit

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const brands =[
-    {
-        id:1, img:"https://i.ibb.co/wMmVwc1/BMW-M-Cars.jpg", brand:"BMW"
-    },
-    {
-        id:2, img:"https://i.ibb.co/W2TR9Bw/1-Mercedes-CLE-Coupe.jpg", brand:"MERCEDES-BENZ"
-    },
-    {
-        id:3, img:"https://i.ibb.co/mD0HRRg/hyundai-elantra-2024-1-768.jpg", brand:"HYUNDAI"
-    },
-    {
-        id:4, img:"https://i.ibb.co/tC3Y4KG/cf576b3f-cbc1-4d4c-87ae.webp", brand:"FERRARI"
-    },
-    {
-        id:5, img:"https://i.ibb.co/NpMwvd0/autentica-cover-m.jpg", brand:"LAMBORGHINI" 
-    },
-    {
-        id:6, img:"https://i.ibb.co/txPP6y7/2015-koenigsegg-one1-first-drive-review-car-and-driver-photo-654233-s-original.jpg", brand:"KOENIGSEGG" 
-    }
+const brands = [
+  {
+    id: 1, img: "https://i.ibb.co/wMmVwc1/BMW-M-Cars.jpg", brand: "BMW"
+  },
+  {
+    id: 2, img: "https://i.ibb.co/W2TR9Bw/1-Mercedes-CLE-Coupe.jpg", brand: "MERCEDES-BENZ"
+  },
+  {
+    id: 3, img: "https://i.ibb.co/mD0HRRg/hyundai-elantra-2024-1-768.jpg", brand: "HYUNDAI"
+  },
+  {
+    id: 4, img: "https://i.ibb.co/tC3Y4KG/cf576b3f-cbc1-4d4c-87ae.webp", brand: "FERRARI"
+  },
+  {
+    id: 5, img: "https://i.ibb.co/NpMwvd0/autentica-cover-m.jpg", brand: "LAMBORGHINI"
+  },
+  {
+    id: 6, img: "https://i.ibb.co/txPP6y7/2015-koenigsegg-one1-first-drive-review-car-and-driver-photo-654233-s-original.jpg", brand: "KOENIGSEGG"
+  }
 ]
 app.use(cors());
 app.use(express.json());
@@ -44,56 +44,71 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const productCollection= client.db("brandDB").collection("products");
+    const productCollection = client.db("brandDB").collection("products");
     const userCollection = client.db("brandDB").collection("user");
-    app.get('/products', async(req, res)=>{
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+
+    app.get('/products', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
-    
-    app.get('/products/:id', async(req, res)=>{
+
+    app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
     })
-  
-    app.post('/products', async(req, res)=>{
-        const newProduct = req.body;
-        console.log(newProduct);
-        const result=await productCollection.insertOne(newProduct);
-        res.send(result)
-    })
-    app.put('/products/:id',async(req, res)=>{
-      const id =req.params.id;
-      const filter={_id:new ObjectId(id)};
-      const options = {upsert:true};
-      const newUpdate= req.body;
-      const product = {
-        $set: {
-          name:newUpdate.name,
-          brand:newUpdate.brand,
-          photo:newUpdate.photo,
-          price:newUpdate.price,
-          description:newUpdate.description,
-          type:newUpdate.type,
-          rating:newUpdate.rating
-        }
-      }
-      const result= await productCollection.updateOne(filter,product,options);
+
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
       res.send(result)
     })
-    app.get('/user', async(req, res)=>{
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const newUpdate = req.body;
+      const product = {
+        $set: {
+          name: newUpdate.name,
+          brand: newUpdate.brand,
+          photo: newUpdate.photo,
+          price: newUpdate.price,
+          description: newUpdate.description,
+          type: newUpdate.type,
+          rating: newUpdate.rating
+        }
+      }
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result)
+    })
+    app.get('/user', async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  })
-    app.post('/user', async(req, res)=>{
+    })
+    app.post('/user', async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+    app.patch('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user);
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          Cart: user.Cart
+        }
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+
+    });
 
 
     // Send a ping to confirm a successful connection
@@ -107,9 +122,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/", (req,res)=>{
-   res.send(brands);
+app.get("/", (req, res) => {
+  res.send(brands);
 })
-app.listen(port, ()=>{
-    console.log(`brand server is running at port : ${port}`)
+app.listen(port, () => {
+  console.log(`brand server is running at port : ${port}`)
 })
